65076f0320d008ab33168992bb4a61f4ac941036
Tommy Ngo <tommyquocngo@gmail.com>
2024-01-24 00:49:22 -0800

    third

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,28 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "./*.{html,js}",
+    
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': {
+          100: '#BA9BC3',
+          200: '#B69DD4',
+        },
+        'beige': {
+          100: '#F6E8B1',
+        },
+        'tBlue': {
+          100: '#1B4965',
+        },
+        
+      },
+      textUnderlineOffset: {
+        6: '6px',
+      }
+    },
+    
   },
   plugins: [],
 }
